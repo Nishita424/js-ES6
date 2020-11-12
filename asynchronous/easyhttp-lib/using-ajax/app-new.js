1c4f46824ae901427571d2f8bae013ff97d5cf8a
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-12 20:52:04 +0530

    Checked with different inputs for put and post

--- asynchronous/easyhttp-lib/using-ajax/app-new.js
+++ asynchronous/easyhttp-lib/using-ajax/app-new.js
@@ -11,17 +11,21 @@ http
   })
   .catch((err) => console.log(err));
 
-const data = {
+const data1 = {
   title: 'New posting',
   userid: 423,
 };
+const data2 = {
+  title: 'Updated posting',
+  userid: 424,
+};
 http
-  .post('https://jsonplaceholder.typicode.com/posts', data)
+  .post('https://jsonplaceholder.typicode.com/posts', data1)
   .then((response) => console.log(response))
   .catch((err) => console.log(err));
 
 http
-  .put('https://jsonplaceholder.typicode.com/posts/1/', data)
+  .put('https://jsonplaceholder.typicode.com/posts/1/', data2)
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
 
